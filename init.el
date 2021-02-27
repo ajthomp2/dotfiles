91405a87afdbdc049494ee3eddc07ac4bb002a4a
Alex Thompson <alex.thompson6@gmail.com>
2021-02-27 15:17:00 -0600

    mostly new additions to emacs config

--- init.el
+++ init.el
@@ -26,14 +26,13 @@
 ;; diminish allows you to remove minor modes from the mode line with :diminish in use-package
 (use-package diminish)
 
-;; I think these were programatically added when I installed the evil plugin
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(treemacs-evil lsp-ui dap-mode lsp-java helm-lsp dap-java flycheck visual-fill-column diminish yasnippet company quickrun which-key helm-descbinds helm projectile use-package auctex evil)))
+   '(all-the-icons doom-themes rustic rainbow-delimiters helm-lsp lsp-ui treemacs-evil lsp-java lsp-mode flycheck yasnippet-snippets yasnippet company visual-fill-column diminish quickrun which-key helm-descbinds helm projectile use-package auctex evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -80,22 +79,11 @@
 (set-default-coding-systems 'utf-8)
 
 ;; enable spell check for text mode and in comments for prog mode
-(setq ispell-program-name "/usr/local/bin/aspell")
+(setq ispell-program-name "/usr/bin/aspell")
 (add-hook 'text-mode-hook 'flyspell-mode)
 ;; (diminish 'flyspell-mode)
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 ;; (diminish 'flyspell-prog-mode)
-
-;; wrap text at 120 characters
-(setq-default fill-column 120)
-
-;; automatic line breaks
-(use-package visual-fill-column
-  ;; :diminish auto-fill-function
-  :init
-  (global-visual-line-mode t)
-  :config
-  (global-visual-fill-column-mode t))
 
 ;; Maintain a list of recent files opened
 (recentf-mode 1)
@@ -103,7 +91,43 @@
 
 ;; enable line numbers. Non-buggy mode was added in Emacs 26
 (when (version<= "26.0.50" emacs-version )
-  (global-display-line-numbers-mode))
+  (global-display-line-numbers-mode t))
+(column-number-mode t)
+
+;; disable line numbers for some mode
+(dolist (mode '(term-mode-hook
+                shell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; move backup files to custom directory to eliminate clutter
+;; "." is a regex that matches everything
+(setq backup-directory-alist `(("." . ,(expand-file-name "tmp-files/backup-files/" user-emacs-directory))))
+
+;; auto-save-mode doesn't create the path automatically
+(make-directory (expand-file-name "tmp-files/auto-save-files/" user-emacs-directory) t)
+
+(setq auto-save-list-file-prefix (expand-file-name "tmp-files/auto-save-files/sessions/" user-emacs-directory)
+      auto-save-file-name-transforms `((".*" ,(expand-file-name "tmp-files/auto-save-files/" user-emacs-directory) t)))
+
+;; ----- ;;
+;; THEME ;;
+;; ----- ;;
+
+(use-package doom-themes
+  :config
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t)
+  (load-theme 'doom-one t)
+
+  (setq doom-themes-treemacs-theme "doom-colors")
+  (doom-themes-treemacs-config)
+
+  (doom-themes-org-config))
+
+;; NOTE: the fist time you load your configuration on a new machine, you'll need to run
+;; the following command interactively so that the mode line icons display correctly
+;; M-x all-the-icons-install-fonts
+(use-package all-the-icons)
 
 ;; --------------- ;;
 ;; CODING SETTINGS ;;
@@ -111,6 +135,21 @@
 
 ;; Automatically add ending brackets and braces
 (electric-pair-mode 1)
+
+;; comment without having to select line
+(defun comment-or-uncomment-region-or-line ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+            (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)))
+(global-set-key (kbd "C-c /") 'comment-or-uncomment-region-or-line)
+
+;; brackets become different colors to see which ones match more easily
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 ;; Make sure tab-width is 4 and not 8
 (setq-default tab-width 4)
@@ -152,12 +191,24 @@
 (use-package which-key
   ;; :diminish which-key-mode
   :init
-  (which-key-mode))
+  (which-key-mode)
+  :config
+  (setq which-key-idle-delay 0.3))
 
 ;; quickrun allows you to execute most programs (if there is language support) in the current buffer
 (use-package quickrun
   :bind
-  ("C-c r" . quickrun))
+  ("C-c r" . quickrun)
+  :config
+  (setq quickrun-focus-p nil ;; disable focusing output buffer after running
+        quickrun-timeout-seconds 180))
+(setq
+   display-buffer-alist
+   `(("^\\(\\*quickrun\\*\\|\\*rustfmt\\*\\|\\*rustic-compilation\\*\\)$"
+      (display-buffer-reuse-window display-buffer-in-previous-window display-buffer-in-side-window)
+      (side . bottom)
+      (window-width . 0.2)
+      (reusable-frames . visible))))
 
 ;; company ("complete anything") is a text completion framework for emacs
 (use-package company)
@@ -181,13 +232,23 @@
    (java-mode . lsp))
   :commands lsp
   :config
-  (setq lsp-completion-enable-additional-text-edit nil)
+  ;; (setq lsp-completion-enable-additional-text-edit nil)
   (define-key lsp-mode-map (kbd "C-c l") lsp-command-map)
   :init
   (setq lsp-keymap-prefix "C-c l" ;; this is for which-key integration documentation
         lsp-completion-provider :capf))
-(use-package lsp-java)
-;;  :config (add-hook 'java-mode-hook 'lsp))
+
+;; LSP Java
+(use-package lsp-java
+  :after lsp-mode)
+
+;; Rust
+(use-package rustic
+  :after lsp-mode
+  :init
+  (setq rustic-lsp-server 'rls))
+(add-hook 'before-save-hook (lambda () (when (eq 'rustic-mode major-mode)
+                                           (rustic-format-buffer))))
 
 ;; DAP is the debug adapter protocol for Emacs
 ;; (use-package dap-mode
